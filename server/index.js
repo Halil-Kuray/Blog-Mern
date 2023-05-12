0bbefd8c57bcbe6b66aa0da54824a712ee1537c8
Halil-Kuray <71791007+Halil-Kuray@users.noreply.github.com>
2023-05-12 18:30:44 +0200

    authController created

--- server/index.js
+++ server/index.js
@@ -2,13 +2,20 @@ const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv').config()
 const cors = require('cors')
+const authController = require('./controllers/authController')
 const app = express()
 
 
-//conect db
+//Conect db
 mongoose.set('strictQuery', false)
 mongoose.connect(process.env.MONGO_URL)
 
-// connect server
+//Routes
+//app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use('/auth', authController)
+
+//Connect server
 
 app.listen(process.env.PORT, () => console.log("server started"))
